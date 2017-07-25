93b66b5f84d54693bcce9a5deca319809a392c32
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-25 17:08:09 -0400

    Created the Face and Suit enum

---
+++ COMP123-s2017-Lesson11B/Face.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Bishnu Khanal
+ * DAte: July 25, 2017
+ * Description: This is a demo for week 11
+ * Verson: 0.1 Created the Face enum
+ */
+namespace COMP123_s2017_Lesson11B
+{
+    public enum Face
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+}
---
+++ COMP123-s2017-Lesson11B/Suit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Bishnu Khanal
+ * DAte: July 25, 2017
+ * Description: This is a demo for week 11
+ * Verson: 0.1 Created the Suit enum
+ */
+namespace COMP123_s2017_Lesson11B
+{
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+}
